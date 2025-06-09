13ec056cd6b5d03c4683b228fdd3b9fcccc55fdb
ionjpg <155106238+ionjpgUwu@users.noreply.github.com>
2025-06-09 13:34:16 -0400

    Cambios

--- reserva/views.py
+++ reserva/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Reserva
 from .forms import ReservaForm
-from 
+
 
 def index(request):
     return render(request,'index.html')
