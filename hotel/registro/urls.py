67a4655a2aadb8d2327d23dc16061e50861e2846
ionjpg <155106238+ionjpgUwu@users.noreply.github.com>
2025-06-09 05:35:28 -0400

    Cerrado de Sesion

--- hotel/registro/urls.py
+++ hotel/registro/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('accounts/logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
-     path('accounts/login/', auth_views.LoginView.as_view(template_name='registro/login.html'), name='login'),
-     
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registro/login.html'), name='login'),
+
 ]
--- hotel/reserva/views.py
+++ hotel/reserva/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Reserva
 from .forms import ReservaForm
-from 
 
 def index(request):
     return render(request,'index.html')
